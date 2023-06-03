80594e56fa03f93da27e4a5b70ed11022194ae7d
Abhijeet Raj <abhijeetraj900@gmail.com>
2023-06-03 22:30:31 +0530

    productList and Store added

--- src/components/e-commerce/productList/ProductList.js
+++ src/components/e-commerce/productList/ProductList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { laodProducts } from '../../../redux/slices/productSlice'
 
 function ProductList() {
+
+  const dispatch = useDispatch()
+  const  products = useSelector((state)=> state.productReducer.products);
+
+  useEffect(()=> {
+    fetchData();
+  }, []);
+  
+async function fetchData() {
+  const response = await fetch('https://api.escuelajs.co/api/v1/products');
+  const data = await response.json();
+  dispatch(laodProducts(data))
+}
+
   return (
-    <div>ProductList</div>
+    <div>products.map{item=> <h4>{item.title}</h4>}</div>
   )
 }
 
-export default ProductList
+export default ProductList;
